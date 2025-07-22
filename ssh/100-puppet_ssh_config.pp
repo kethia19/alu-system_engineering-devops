10e8a98d80c7499abe5b52e6c2e1aea7905fbb60
kethia19 <k.kayigire@alustudent.com>
2025-07-22 10:00:07 -0700

    New cahnges added

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,17 @@
-# Setting up my client config file
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  replace => true,
-}
-
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
+# Setting up my client config file 
+include stdlib 
+ 
+file_line { 'Turn off passwd auth': 
+  ensure => present, 
+  path   => '/etc/ssh/ssh_config', 
+  line   => '    PasswordAuthentication no', 
+  replace => true, 
+} 
+ 
+file_line { 'Declare identity file': 
+  ensure => present, 
+  path   => '/etc/ssh/ssh_config', 
+  line   => '    IdentityFile ~/.ssh/school', 
+  replace => true, 
 }
 
